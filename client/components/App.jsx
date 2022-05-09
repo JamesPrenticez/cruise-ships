15891b389d3b27866f640da3baf063dfa843ab55
Robert Wilkinson <robert@MacBook-Air.local>
2022-05-09 15:36:57 +1200

    New Reviews Part Branch setup -wth App Component ready for Routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,55 +1,16 @@
-import React, {useState, useEffect} from 'react'
-import { getShips } from '../api/ships'
-import { getReviews } from '../api/reviews'
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-function App() {
-  const [ships, setShips] = useState([])
-  const [reviews, setReviews] = useState([])
 
-  useEffect(() => {
-    getShips()
-      .then((res) => {
-        setShips(res)
-      })
-  }, [])
-
-  const handleClickReviews = (id) => {
-    getReviews(id)
-    .then((res) => {
-      setReviews(res)
-    })
-  }
-
-  console.log(reviews)
-
-  return (
-    <div>
-      <h1 className='text-7xl text-sky-500'>
-        Cruise Ships of the World!
-      </h1>
-
-      {ships.map((ship) => (
-        <div key={ship.id}>
-          <h1>{ship.name}</h1>
-          <button 
-            className='h-12 w-24 bg-green-500'
-            onClick={() => handleClickReviews(ship.id)}
-          >
-            Reviews
-          </button>
-
-          {reviews.length > 0 && 
-            reviews.map((review) => (
-              <p>{review.content}</p>
-            ))
-          }
-        </div>
-      ))}
-
-      {/* <img className='h-64 w-128'
-        src='images/default.webp'
-      /> */}
-    </div>
+function App()  {
+  return  (
+    <>
+    <BrowserRouter>
+      <Routes>
+        {/* Put Routes here for Reviews Part */}
+      </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
